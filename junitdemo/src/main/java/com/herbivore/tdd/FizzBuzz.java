f25205e6d8c3936c0157235d56f75545e8792c3c
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-03-24 10:11:18 +0800

    tdd refactor

--- junitdemo/src/main/java/com/herbivore/tdd/FizzBuzz.java
+++ junitdemo/src/main/java/com/herbivore/tdd/FizzBuzz.java
@@ -2,6 +2,27 @@ package com.herbivore.tdd;
 
 public class FizzBuzz {
 
+	// Refactored version
+	public static String compute(int n) {
+		StringBuilder result = new StringBuilder();
+
+		if (n % 3 == 0 && n % 5 == 0) {
+			result.append("FizzBuzz");
+		}
+		else if (n % 3 == 0) {
+			result.append("Fizz");
+		}
+		else if (n % 5 == 0) {
+			result.append("Buzz");
+		}
+		else {
+			result.append(n);
+		}
+
+		return result.toString();
+	}
+
+/*
 	public static String compute(int n) {
 
 		if (n % 3 == 0 && n % 5 == 0) {
@@ -20,4 +41,5 @@ public class FizzBuzz {
 			return String.valueOf(n);
 		}
 	}
+*/
 }
