081e4c23c90f5976454a403f4d735771c1635e8d
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-04-05 14:56:33 +0800

    non-public fields accessing & methods invocation

--- spring-boot-unit-testing-demo/src/main/resources/static/note.md
+++ spring-boot-unit-testing-demo/src/main/resources/static/note.md
@@ -3,3 +3,11 @@
 ## ReflectionTestUtils.class
 
 * Spring's support for testing non-public components with the use of reflection
+
+### Methods
+
+* Accessing fields:
+    - `ReflectionTestUtils.getField(obj, fieldName);`
+    - `ReflectionTestUtils.setField(obj, fieldName, setValue);`
+* Invoking Methods:
+    - `ReflectionTestUtils.invokeMethod(obj, methodName)`
--- spring-boot-unit-testing-demo/src/test/java/com/herbivore/test/ReflectionTestUtilsTest.java
+++ spring-boot-unit-testing-demo/src/test/java/com/herbivore/test/ReflectionTestUtilsTest.java
@@ -4,9 +4,15 @@ import com.herbivore.component.MvcTestingExampleApplication;
 import com.herbivore.component.models.CollegeStudent;
 import com.herbivore.component.models.StudentGrades;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = MvcTestingExampleApplication.class)
 public class ReflectionTestUtilsTest {
@@ -26,5 +32,29 @@ public class ReflectionTestUtilsTest {
 		studentOne.setLastname("Hellscream");
 		studentOne.setEmailAddress("for-the-horde@blizzard.com");
 		studentOne.setStudentGrades(grades);
+
+		ReflectionTestUtils.setField(studentOne, "id", 1);
+		ReflectionTestUtils.setField(studentOne, "studentGrades",
+				new StudentGrades(Arrays.asList(100.0, 85.0, 76.50, 91.75)));
+	}
+
+	@Test
+	void getPrivateField() {
+		final int expected = 9999999;
+//		final int expected = 1;
+		final Integer actual = (Integer)
+				ReflectionTestUtils.getField(studentOne, "id");
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void invokePrivateMethod() {
+		final String expected = "Garrosh 9999999";
+//		final String expected = "Garrosh 1";
+		final String actual = ReflectionTestUtils.invokeMethod(
+				studentOne, "getFirstNameAndId");
+
+		assertEquals(expected, actual, "Fail private method not call");
 	}
 }
