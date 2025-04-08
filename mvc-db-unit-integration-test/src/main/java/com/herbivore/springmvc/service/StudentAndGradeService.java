197f63b7bef37d31ba4bb0d123cd772116d6c5fd
paraaaasaur <79886141+paraaaasaur@users.noreply.github.com>
2025-04-08 08:01:40 +0800

    h2 sample data setup/cleanup
    big patch 2 rename Service#isStudentFound
    
    polish

--- mvc-db-unit-integration-test/src/main/java/com/herbivore/springmvc/service/StudentAndGradeService.java
+++ mvc-db-unit-integration-test/src/main/java/com/herbivore/springmvc/service/StudentAndGradeService.java
@@ -5,6 +5,8 @@ import com.herbivore.springmvc.repository.StudentDao;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional
 public class StudentAndGradeService {
@@ -20,5 +22,9 @@ public class StudentAndGradeService {
 		CollegeStudent student = new CollegeStudent(firstname, lastname, emailAddress);
 		student.setId(0);
 		studentDao.save(student);
+	}
+
+	public boolean isStudentFound(int id) {
+		return studentDao.findById(id).isPresent();
 	}
 }
--- mvc-db-unit-integration-test/src/main/resources/static/note.md
+++ mvc-db-unit-integration-test/src/main/resources/static/note.md
@@ -5,3 +5,11 @@
 * Embedded, in-memory db (like H2) is good to go by just adding to dependency.
   - No need for manual connection setup 
   - Further customization is feasible as well
+
+### Sample Data
+
+#### Set up & clean up
+
+* Where? → `@beforeEach` and `@afterEach` methods
+* How? → **`jdbcTemplate.execute()`** + SQL statements
+  - A helper class in Spring framework, to provide handy JDBC-level operations
